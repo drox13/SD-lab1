07a953b17ff9a5831e4f8ca4452534c0f3f0b4c7
drox13 <dario.baron@uptc.edu.co>
2021-08-18 16:15:17 -0500

    se agrregaron documentos .ps1

---
+++ bash/buid.ps1
@@ -0,0 +1,7 @@
+docker build -t instancias .
+docker run -d --name server1 -p 8002:8000 instancias
+docker run -d --name server2 -p 8003:8000 instancias
+cd ..
+cd BashMiddleware
+docker build -t middlewareimage .
+docker run -d --name middleware -p 8001:8000 middlewareimage
---
+++ bash/deleteAll.ps1
@@ -0,0 +1,4 @@
+docker stop server1 server2 middleware
+docker rm server1 server2 middleware
+docker rmi instancias
+docker rmi middlewareimage
