60a3ccc1e7848101bb4db0865a1822dde3d17b22
drox13 <dario.baron@uptc.edu.co>
2021-08-18 00:13:12 -0500

    se modifico el ping

--- bash/ping.sh
+++ bash/ping.sh
@@ -1,6 +1,7 @@
-curl -I http://172.17.0.2:8002/status | cat - /home/lab1/server1.log > temp && mv temp /home/lab1/server1.log
-curl -I http://172.17.0.3:8003/status | cat - /home/lab1/server2.log > temp && mv temp /home/lab1/server2.log
+curl -I http://172.17.0.2:8000/status | cat - /home/lab1/server1.txt > temp && mv temp /home/lab1/server1.txt
+curl -I http://172.17.0.3:8000/status | cat - /home/lab1/server2.txt > temp && mv temp /home/lab1/server2.txt
 
+#curl -I http://192.168.100.16/status | cat - /home/debian2/debian0_ping.txt > temp && mv temp /home/debian2/debian0_ping.txt
 #curl -I http://192.168.100.16/status | cat - /home/debian2/debian0_ping.txt > temp && mv temp /home/debian2/debian0_ping.txt
 
 # if ping -c1 172.17.0.3 &>/dev/null;
