7b7ef0d6fee73c00d23218c735151d974bbaeb87
drox13 <dario.baron@uptc.edu.co>
2021-08-18 17:10:21 -0500

    se arreglo el path para guadar los logs

--- bash/ping.sh
+++ bash/ping.sh
@@ -1,23 +1,2 @@
-curl -I 172.17.0.2:8000/status | cat - /home/lab1/SD-Lab1/Milldware/server1.txt > temp && mv temp /home/lab1/SD-Lab1/Milldware/server1.txt
-curl -I 172.17.0.3:8000/status | cat - /home/lab1/SD-Lab1/Milldware/server2.txt > temp && mv temp /home/lab1/SD-Lab1/Milldware/server2.txt
-
-#curl -I http://192.168.100.16/status | cat - /home/debian2/debian0_ping.txt > temp && mv temp /home/debian2/debian0_ping.txt
-#curl -I http://192.168.100.16/status | cat - /home/debian2/debian0_ping.txt > temp && mv temp /home/debian2/debian0_ping.txt
-
-# if ping -c1 172.17.0.3 &>/dev/null;
-# then
-#         echo "Instancia 1 online";
-#         date > log.txt;
-#         $@
-# else
-#         echo "Instancia 1 No tiene conexion" > log.txt;
-# fi
-
-# if ping -c1 172.17.0.4 &>/dev/null;
-# then
-#         echo "Instancia 2 online";
-#         date
-#         $@
-# else
-#         echo "Instancia 2 No tiene conexion";
-# fi
+curl -I 172.17.0.2:8000/status | cat - /home/lab1/SD-lab1/Milldware/server1.txt > temp && mv temp /home/lab1/SD-lab1/Milldware/server1.txt
+curl -I 172.17.0.3:8000/status | cat - /home/lab1/SD-lab1/Milldware/server2.txt > temp && mv temp /home/lab1/SD-lab1/Milldware/server2.txt
