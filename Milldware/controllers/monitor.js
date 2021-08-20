aa2414a3e7b674589d1ea1c521f764d4da914e37
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2021-08-19 20:48:37 -0500

    Test: Implementacion de hbs para renderizar del lado del server y mostrar datos de forma dinamica en el cliente

--- Milldware/controllers/monitor.js
+++ Milldware/controllers/monitor.js
@@ -1,7 +1,44 @@
-
-
-const getLogs = (req, res) => {
-
+function writeLogs() {
+    setInterval(() => {
+        shell.exec('/home/lab1/SD-lab1/bash/ping.sh');
+        //prueba Dro
+        //shell.exec('/home/dario/Documentos/SD-lab1/Milldware/pingdro.sh')
+    }, 1000);
 }
 
-module.exports = getLogs
+readLastLogServer1() {
+    setInterval(() => {
+        try {
+            let server1 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
+            //Ruta DRO
+            //const server1 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
+            console.log(server1);
+        } catch (err) {
+            console.error(err);
+        }
+    }, 1000);
+}
+
+function readLastLogServer2() {
+    setInterval(() => {
+        try {
+            const server2 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
+            //ruta DRO
+            //const server2 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
+            console.log(server2);
+        } catch (err) {
+            console.error(err);
+        }
+    }, 1000);
+}
+
+const getLogs = (req, res) => {
+	res.render('home',{
+        info: server1,
+    });
+};
+
+writeLogs();
+readLastLogServer1();
+
+module.exports = getLogs;
