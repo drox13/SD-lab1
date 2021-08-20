ab7260f62992226bdb56c1b4a3eebb2e0ab3fc3b
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2021-08-19 21:25:12 -0500

    sfs

--- Milldware/controllers/monitor.js
+++ Milldware/controllers/monitor.js
@@ -1,36 +1,32 @@
-function writeLogs() {
-	setInterval(() => {
-		shell.exec('/home/lab1/SD-lab1/bash/ping.sh');
-		//prueba Dro
-		//shell.exec('/home/dario/Documentos/SD-lab1/Milldware/pingdro.sh')
-	}, 1000);
-}
+let writeLogs = setInterval(() => {
+	shell.exec('/home/lab1/SD-lab1/bash/ping.sh');
+	//prueba Dro
+	//shell.exec('/home/dario/Documentos/SD-lab1/Milldware/pingdro.sh')
+}, 1000);
 
-function readLastLogServer1() {
-	setInterval(() => {
-		try {
-			let server1 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
-			//Ruta DRO
-			//const server1 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
-			console.log(server1);
-		} catch (err) {
-			console.error(err);
-		}
-	}, 1000);
-}
+let readLogServer1 = setInterval(() => {
+	try {
+		let server1 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
+		//Ruta DRO
+		//const server1 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
+		console.log(server1);
+	} catch (err) {
+		console.error(err);
+	}
+}, 1000);
 
-function readLastLogServer2() {
-	setInterval(() => {
-		try {
-			const server2 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
-			//ruta DRO
-			//const server2 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
-			console.log(server2);
-		} catch (err) {
-			console.error(err);
-		}
-	}, 1000);
-}
+// function readLastLogServer2() {
+// 	setInterval(() => {
+// 		try {
+// 			const server2 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
+// 			//ruta DRO
+// 			//const server2 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
+// 			console.log(server2);
+// 		} catch (err) {
+// 			console.error(err);
+// 		}
+// 	}, 1000);
+// }
 
 const getLogs = (req, res) => {
 	res.render('home', {
@@ -39,7 +35,7 @@ const getLogs = (req, res) => {
 };
 
 writeLogs();
-readLastLogServer1();
+readLogServer1();
 //readLastLogServer2();
 
 module.exports = getLogs;
