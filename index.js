1ef9680fd59036dd2c2942f733fc4a4b74e55bf7
drox13 <dario.baron@uptc.edu.co>
2021-08-19 22:27:08 -0500

    se agrego el servicio status en las instancias

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ app.get('/', function(req, res){
 app.listen(port, () => {
    console.log(`Example app listening at http://localhost:${port}`);
  });
+
+ function between(min, max) {  
+   return Math.floor(
+     Math.random() * (max - min) + min
+   )
+ }
+ 
+ app.get('/status', (req, res) => {
+   if(between(1,5) == 3) {
+     res.sendStatus(500)
+   } else {
+     res.sendStatus(200)
+   }
+ })
