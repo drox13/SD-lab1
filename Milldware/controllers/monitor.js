0a7f436bea813211d97654cfc6374c8f76f582e6
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2021-08-20 23:07:41 -0500

    Se refactorizaron los metodos de leer el archivo de logs, se creo el metodo de checkStatus para validar el codigo que llega desde las instancias.

--- Milldware/controllers/monitor.js
+++ Milldware/controllers/monitor.js
@@ -1,50 +1,48 @@
-const shell = require('shelljs')
-const fs = require('fs')
+const shell = require('shelljs');
+const fs = require('fs');
 
-function writeLogs(){
+var myServer = '';
+
+function writeLogs() {
 	setInterval(() => {
-		//shell.exec('/home/lab1/SD-lab1/bash/ping.sh');
+		shell.exec('/home/lab1/SD-lab1/bash/ping.sh');
 		//prueba Dro
-		shell.exec('/home/dario/Documentos/SD-lab1/Milldware/pingdro.sh')
+		//shell.exec('/home/dario/Documentos/SD-lab1/Milldware/pingdro.sh');
 	}, 1000);
 }
-var server1 = "";
-function readLogServer1(){
-	setInterval(() => {
-	   try {
-		   //server1 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
-		   //Ruta DRO
-		   server1 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer1.log', 'utf8');
-		   console.log(server1);
-	   } catch (err) {
-		   console.error(err);
-	   }
-   }, 1000);
-}
 
-var server2 =""
-function readLastLogServer2() {
+function readLogServer(path) {
 	setInterval(() => {
 		try {
-			//server2 = fs.readFileSync('/home/lab1/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
-			//ruta DRO
-			server2 = fs.readFileSync('/home/dario/Documentos/SD-lab1/Milldware/lastLogServer2.log', 'utf8');
-			console.log(server2);
+			myServer = fs.readFileSync(path, 'utf8');
+			//Ruta DRO
+			//myServer = fs.readFileSync(path, 'utf8');
+			checkStatus(myServer);
+			console.log(myServer);
 		} catch (err) {
 			console.error(err);
 		}
 	}, 1000);
 }
 
+function checkStatus(server) {
+	let data = server.split(' ');
+	for (let index = 0; index < data.length; index++) {
+		const status = data[0];
+		if (status != 200) {
+		}
+	}
+}
+
 const getLogs = (req, res) => {
 	res.render('home', {
 		infoServer1: server1,
-		infoServer2: server2
+		infoServer2: server2,
 	});
 };
 
 writeLogs();
-readLogServer1();
-readLastLogServer2();
+readLogServer('/home/lab1/SD-lab1/Milldware/lastLogServer1.log');
+readLogServer('/home/lab1/SD-lab1/Milldware/lastLogServer2.log');
 
 module.exports = getLogs;
